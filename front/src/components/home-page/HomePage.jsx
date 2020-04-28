0fbcb9937abe70d241fc90e88221418109b5d816
Willy Cochin <willy.cochin@gmail.com>
2020-04-28 14:58:55 +0200

    refact Product list

--- front/src/components/home-page/HomePage.jsx
+++ front/src/components/home-page/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
                 to={{
                   pathname: '/product-list',
                   state: {
-                    titleList: 'Sodas'
+                    titleList: 'Softs'
                   }
                 }}
               >
